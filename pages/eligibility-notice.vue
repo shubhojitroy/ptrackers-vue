2f170322e0dfaf9696a426c5dcaad39495b95080
Shubhojit Roy <subo.roy@boardroomlimited.com.au>
2018-06-27 08:39:55 +1000

    Updated style for eligibility notice period.

--- pages/eligibility-notice.vue
+++ pages/eligibility-notice.vue
@@ -1,5 +1,7 @@
 <template>
-  <section class="section">
+  <div>
+    <div class="container">
+      <section class="canvas">
     <div class="container box">
       <h1 class="title">Legal Restrictions</h1>
       <div class="content">
@@ -18,6 +20,8 @@
       </nuxt-link><br>
     </div>
   </section>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,5 +31,27 @@
 </script>
 
 <style scoped>
-
+.canvas {
+  z-index: 50;
+  /* padding: 1rem; */
+  background: #fff 50%;
+  background-size: cover;
+  border-radius: 1px;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.02);
+  transition: all 0.5s ease;
+}
+.alert-bound {
+  width: 100%;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+  padding: 1.0rem;
+}
 </style>
