07e30b141b224563e71c46b71b22b4290c3f0fad
Shubhojit Roy <subo.roy@boardroomlimited.com.au>
2018-06-27 10:55:04 +1000

    Created end page.

---
+++ pages/end-page.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="container">
+      <section class="canvas">
+        <h1 class="title is-size-2 is-size-3-mobile">
+          Application Completed
+        </h1>
+        <div class="alert-bound">
+          <p>
+            Your application details have been registered with us. Kindly click the button to go back to the application start page.
+          </p>
+        </div>
+
+        <div class="space"></div>
+
+        <div class="control">
+            <nuxt-link to="/" class="button is-primary">
+                close&nbsp;&nbsp;<span class="icon"><i class="mdi mdi-arrow-left-bold-hexagon-outline"></i></span>
+            </nuxt-link>
+        </div>
+
+       </section>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.canvas {
+  z-index: 50;
+  padding: 1rem;
+  background: #fff 50%;
+  background-size: cover;
+  border-radius: 1px;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.02);
+  transition: all 0.5s ease;
+}
+h4 {
+  font-size: 1.33rem;
+  margin: 0.75em 0;
+  font-weight: 500;
+}
+ul {
+  list-style-type: disc;
+}
+li {
+  line-height: 1.5;
+  padding: 0.5rem 1rem;
+  margin: 0 1.25em;
+}
+p {
+  line-height: 1.75rem;
+}
+.space {
+  display: block;
+  width: 100%;
+  height: 1.5em;
+}
+.alert-bound {
+  width: 100%;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+  padding: 1.0rem;
+
+}
+.footnote {
+  font-size: 0.75rem;
+  line-height: 1.2em;
+}
+</style>
