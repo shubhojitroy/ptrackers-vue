88d46c9b69eb4e634cde6f671ec2cbae90693a8e
Colin Johnsun <colin.johnsun@boardroomlimited.com.au>
2018-06-28 19:26:41 +1000

    Fixed Country selector.

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,35 @@
 <template>
   <div>
-    <div class="container">
+    <div class="container content">
       <section class="canvas">
-        <h1 class="title is-size-2 is-size-3-mobile">
+        <h1 class="title">
           PM Capital Global Opportunities Fund Limited<br>PM Capital <span style="color: red;">GO 2025</span> Limited
         </h1>
         <p>
           Portfolio Tracking Exchangeable Redeemable Securities (Converting Security) (known as ‘<span style="color: red;">PTrackERS</span>’)
         </p>
-        <div class="space"></div>
         <div class="alert-bound">
           <p>
             The PTrackERS ("Offer") is being made to any person who has a registered address in Australia or New Zealand.
           </p>
           <p>
-            Due to legal restrictions, access to these webpages is only intended for access by persons within Australia and New Zealand. These webpages do not constitute an offer of securities in any place in which, or to any person to whom, it would not be lawful to make such an offer.
+            Due to legal restrictions, access to these webpages is only intended for access by persons within Australia and New Zealand.
+            These webpages do not constitute an offer of securities in any place in which, or to any person to whom, it would not be lawful to make such an offer.
           </p>
-          <div><!--class="columns"-->
-            <div><!--class="column is-two-thirds-tablet is-half-desktop"-->
+          <div class="columns">
+            <div class="column is-two-thirds-tablet is-half-desktop">
               <form novalidate @submit.prevent="submitForm">
                 <b-field label="Country of Primary Residence" :type="countryType" :message="countryMessage">
                   <b-autocomplete v-model="country" field="name" :placeholder="placeholder" :data="filteredCountries" open-on-focus keep-first
                     icon="earth"
-                    @select="option => selected = option"
+                    @select="onSelect"
                     @focus="focusSelect"
-                    @blur="$v.country.$touch()">
+                    @blur="leaveSelect">
+                    <!-- <template slot="header">
+                      <a @click="clearSelect">
+                          <span> Clear </span>
+                      </a>
+                    </template> -->
                     <template slot="empty">No results found</template>
                   </b-autocomplete>
                 </b-field>
@@ -57,6 +62,7 @@ export default {
     return {
       name: "",
       country: "",
+      country2: "",
       selected: null,
       placeholder: _placeholder,
       countries: countries.items
@@ -92,15 +98,29 @@ export default {
         this.$router.push(validCountry);
       }
     },
+    clearSelect () {
+      this.placeholder = this.country;
+      this.country = '';
+    },
     focusSelect () {
       this.placeholder = this.country;
       this.country = '';
     },
+    onSelect (option) {
+      this.selected = option;
+      console.log('selected!!', option && option.name)
+    },
     leaveSelect () {
+      console.log(`leaving!!!! country=${this.country}, placeholder=${this.placeholder}`, this.selected)
       if (this.country === '' && this.placeholder !== _placeholder) {
-        this.country == this.placeholder;
+        this.country2 = this.placeholder;
       }
-    }
+      let vm = this;
+      setTimeout(function() {
+        if (vm.country === '') { vm.country = vm.country2; }
+      },200)
+    //  this.$v.country.$touch()
+    },
   }
 }
 </script>
