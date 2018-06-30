906a2ff7eeab66a7a0f17fe7db86a16475554598
Colin Johnsun <colin.johnsun@boardroomlimited.com.au>
2018-06-30 13:25:09 +1000

    Logos on the Nav Bar

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="navbar-brand">
         <a class="navbar-item" href="http://www.pmcapital.com.au/global-companies-fund">
-          <img src="~assets/img/pmcaptalgo2025limited.png" alt="PM Capital Limited">
+          <img src="~assets/img/pm-capital-go2025.png" alt="PM Capital Limited">
         </a>
         <!-- <div class="navbar-burger" @click="showNav = !showNav" :class="{ 'is-active': showNav }">
           <span></span>
@@ -11,13 +11,13 @@
           <span></span>
         </div> -->
       </div>
-      <!-- <div class="navbar-menu has-text-left" :class="{ 'is-active': showNav }">
+      <!-- <div class="navbar-menu has-text-left" :class="{ 'is-active': showNav }"> -->
         <div class="navbar-brand">
           <a class="navbar-item" href="http://www.pmcapital.com.au/global-companies-fund">
-            <img src="~assets/img/pmcaptalgo2025limited.png" alt="PM Capital Limited">
+            <img src="~assets/img/pm-capital-global.png" alt="PM Capital Limited">
           </a>
         </div>
-      </div> -->
+      <!-- </div> -->
     </div>
   </nav>
 </template>
@@ -51,40 +51,45 @@
   }
 </script>
 
-<style lang="scss">
-  $brand-color-1: #4A67AB;
-  $navigation-bottom-border: #76c4e3;
-  $brand-color-1-highlight: saturate($brand-color-1, 20%);
+<style lang="scss" scoped>
+$brand-color-1: #4A67AB;
+$navigation-bottom-border: #76c4e3;
+$brand-color-1-highlight: saturate($brand-color-1, 20%);
 
-  $header-height: 4.5rem;
+$header-height: 4.5rem;
 
-  a.navbar-item {
-    color: $brand-color-1;
+.container {
+  display: flex !important;
+  align-items: center !important;
+  justify-content: space-between !important;
+}
+a.navbar-item {
+  color: $brand-color-1;
 
-    &:hover {
-      color: $brand-color-1-highlight;
-    }
+  &:hover {
+    color: $brand-color-1-highlight;
   }
+}
 
-  @media screen and (min-width: 1024px) {
-    html.has-navbar-fixed-top {
-      padding-top: $header-height;
-    }
-    nav.navbar {
-      transition: all 0.2s ease-in;
-      height: 3.25rem;
-      border-bottom: $navigation-bottom-border 2px solid;
-    }
+@media screen and (min-width: 1024px) {
+  html.has-navbar-fixed-top {
+    padding-top: $header-height;
+  }
+  nav.navbar {
+    transition: all 0.2s ease-in;
+    height: 3.25rem;
+    border-bottom: $navigation-bottom-border 2px solid;
+  }
+  .navbar-item img {
+    transition: all 0.2s ease-in;
+    max-height: 2rem;
+  }
+  nav.grow {
+    height: $header-height;
+
     .navbar-item img {
-      transition: all 0.2s ease-in;
-      max-height: 2rem;
-    }
-    nav.grow {
-      height: $header-height;
-
-      .navbar-item img {
-        max-height: 3rem;
-      }
+      max-height: 3rem;
     }
   }
+}
 </style>
